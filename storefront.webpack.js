d3885c5d2dcb6c954662484ce9d4cdfd9fc384d2
Matheus <matheus@e-com.club>
2020-05-06 22:26:09 -0300

    chore: update v2 guia tamanho

--- storefront.webpack.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  resolve: {
-    alias: {
-      './pages/@/product-block.ejs$':
-        require('path').resolve(__dirname, 'template/pages/@/product-block.ejs')
-    }
-  }
-}
