0c8788589982d8466439108b843ef619667065d1
Matheus <matheus@e-com.club>
2020-05-06 21:40:33 -0300

    chore: update to v2

---
+++ storefront.webpack.js
@@ -0,0 +1,8 @@
+module.exports = {
+  resolve: {
+    alias: {
+      './pages/@/product-block.ejs$':
+        require('path').resolve(__dirname, 'template/pages/@/product-block.ejs')
+    }
+  }
+}
